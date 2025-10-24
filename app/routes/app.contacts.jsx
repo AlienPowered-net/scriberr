19d7f2ea7806f5b1676615594dba00193a860eaf
Dylan Sauriol <alienpowered.net@gmail.com>
2025-10-24 14:32:19 -0700

    Fix contacts page footer position - move footer to bottom of page instead of top

--- app/routes/app.contacts.jsx
+++ app/routes/app.contacts.jsx
@@ -4611,54 +4611,54 @@ export default function ContactsPage() {
         )}
       </div>
 
-      {/* Copyright Footer */}
-      <div style={{
-        position: "relative",
-        bottom: "0",
-        left: "0",
-        right: "0",
-        backgroundColor: "#f8f9fa",
-        borderTop: "1px solid #e1e3e5",
-        padding: "16px 24px",
-        marginTop: "40px",
-        fontSize: "14px",
-        color: "#6d7175",
-        zIndex: 100,
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}>
-        <div>
-          © 2025, Scriberr Powered by{" "}
-          <a 
-            href="https://www.alienpowered.net" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            style={{
-              color: "#008060",
-              textDecoration: "none",
-              fontWeight: "600",
-              transition: "color 0.2s ease"
-            }}
-            onMouseEnter={(e) => {
-              e.target.style.color = "#008000";
-            }}
-            onMouseLeave={(e) => {
-              e.target.style.color = "#008060";
-            }}
-          >
-            Aliens
-          </a>
-        </div>
-        <div style={{ 
-          fontStyle: "italic", 
-          fontSize: "12px", 
-          color: "#9ca3af",
-          marginLeft: "24px"
-        }}>
-          {version}
-        </div>
+    {/* Copyright Footer */}
+    <div style={{
+      position: "relative",
+      bottom: "0",
+      left: "0",
+      right: "0",
+      backgroundColor: "#f8f9fa",
+      borderTop: "1px solid #e1e3e5",
+      padding: "16px 24px",
+      marginTop: "40px",
+      fontSize: "14px",
+      color: "#6d7175",
+      zIndex: 100,
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center"
+    }}>
+      <div>
+        © 2025, Scriberr Powered by{" "}
+        <a 
+          href="https://www.alienpowered.net" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{
+            color: "#008060",
+            textDecoration: "none",
+            fontWeight: "600",
+            transition: "color 0.2s ease"
+          }}
+          onMouseEnter={(e) => {
+            e.target.style.color = "#008000";
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.color = "#008060";
+          }}
+        >
+          Aliens
+        </a>
       </div>
+      <div style={{ 
+        fontStyle: "italic", 
+        fontSize: "12px", 
+        color: "#9ca3af",
+        marginLeft: "24px"
+      }}>
+        {version}
+      </div>
+    </div>
     </>
   );
 }
